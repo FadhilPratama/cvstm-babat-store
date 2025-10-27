07184e0dcea2452a70dfbc3231985ff48eb106bc
FadhilPratama <pratamamuhammadfadhil@gmail.com>
2025-10-27 12:06:08 +0700

    frontend apps tahap 1

--- components/ui/icon-button.tsx
+++ components/ui/icon-button.tsx
@@ -1,18 +1,16 @@
-import {cn} from "@/lib/util";
-import React, {MouseEventHandler} from "react";
+import { cn } from "@/lib/util";
+import React, { MouseEventHandler } from "react";
 
 interface IconButtonProps {
-    onclick?: MouseEventHandler<HTMLButtonElement> | undefined;
+    onClick?: MouseEventHandler<HTMLButtonElement>;
     icon: React.ReactElement;
     className?: string;
 }
 
-const IconButton: React.FC<IconButtonProps> = ({
-                                                   onclick, icon, className
-                                               }) => {
+const IconButton: React.FC<IconButtonProps> = ({ onClick, icon, className }) => {
     return (
         <button
-            onClick={onclick}
+            onClick={onClick}
             className={cn(
                 "rounded-full flex items-center justify-center bg-white border shadow-md p-2 hover:scale-110 transition",
                 className
@@ -21,6 +19,6 @@ const IconButton: React.FC<IconButtonProps> = ({
             {icon}
         </button>
     );
-}
+};
 
 export default IconButton;
